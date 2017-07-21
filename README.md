6e281143354bee7c54aae18a8f7d672e7c48eea5
jian.huang <1342042894@qq.com>
2017-07-21 19:38:19 +0800

    modify readme.md

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # JianPublic
 Collect some code snippets
+
+list.h : Get from linux kernel 2.4.0 .
+
+
+
--- common.h
+++ common.h
@@ -14,7 +14,6 @@ int iThreeMin(int a, int b, int c);
 
 void vSwap(uint8_t & a, uint8_t & b);
 
-//从小到大排序
 void vBubbleSort(uint8_t ai[], uint8_t ucSize);
 
 void vRandomArray(uint8_t aucArray[], uint8_t ucArraySize, uint16_t usRandSeed);
